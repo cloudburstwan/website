2609554f95a4f42d4a4a8289becc71371c3309ce
CloudburstSys <cloudburst@cloudburst.lgbt>
2025-05-28 17:49:22 +0100

    add button to go to href (assumed blog)

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,43 +2,64 @@
     export let status: "hidden" | "info" | "warning" | "danger" = "info";
 
     let color = "rgba(206, 186, 255, 0.68)"
-    let textColor = "rgba(0,0,0,1)";
+    let borderColor = "rgba(102, 93, 128, 0.68)"
+    let hidden: boolean | "" | "until-found" | null | undefined = "";
 
     switch (status) {
         case "warning":
             color = "rgba(255,244,116,0.68)";
+            borderColor = "rgba(128,122,57,0.68)";
             break;
         case "danger":
             color = "rgba(255,186,186,0.68)";
-            break;
-        case "hidden":
-            color = "rgba(0,0,0,0)";
+            borderColor = "rgba(128,93,93,0.68)";
             break;
         case "info":
         default:
             color = "rgba(206, 186, 255, 0.68)";
+            borderColor = "rgba(102, 93, 128, 0.68)";
             break;
     }
 
     if (status == "hidden")
-        textColor = "rgba(0,0,0,0)";
+        hidden = true;
 </script>
 
-<div style="background-color: {color} !important; color: {textColor} !important;" id="top">
-    <slot>&nbsp;</slot>
-    <slot name="detail"></slot>
-</div>
+<header {hidden} style="background-color: {color} !important; border-color: {borderColor} !important;" id="top">
+    <div class="content"><slot>&nbsp;</slot></div>
+    {#if $$props.href}
+        <a href="{$$props.href}" style="border-color: {borderColor} !important;" target="_blank">Click to view blog post</a>
+    {/if}
+</header>
 
 <style>
-    div {
+    header {
         position: absolute;
         top: 5px;
         left: 5px;
         width: calc(100vw - 10px);
-        padding: 10px;
         background-color: rgba(206, 186, 255, 0.68) !important;
+        border: solid 2px rgba(102, 93, 128, 0.68);
         border-radius: 20px;
         text-align: center;
-        font-weight: bold;
+        font-weight: bolder;
+        color: black;
+    }
+
+    header .content {
+        padding: 10px;
+    }
+
+    a {
+        display: block;
+        border-top: solid 2px rgba(102, 93, 128, 0.68);
+        width: 100%;
+        font-size: 1em !important;
+        text-decoration: none !important;
+        background: transparent;
+        color: black;
+        font-family: inherit;
+        font-weight: inherit;
+        cursor: pointer;
     }
 </style>
